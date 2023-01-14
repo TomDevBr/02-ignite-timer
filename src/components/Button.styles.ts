4eabfb73ffae563494a90ba6ac5326931e904791
TomDevBr <tomasjocalheiros@gmail.com>
2023-01-14 09:41:33 -0300

    Estrutura da aplicação - Configurando ESLint

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -6,12 +6,6 @@ interface ButtonContainerProps {
   variant: ButtonVariant;
 }
 
-const ButtonVariants = {
-  primary: "purple",
-  secondary: "orange",
-  danger: "red",
-  success: "green",
-};
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
@@ -19,6 +13,6 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border: 0;
   margin: 8px;
 
-  background-color: ${props => props.theme["green-500"]};
-  color: ${props => props.theme.white}
+  background-color: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme.white};
 `;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,7 +14,7 @@ export const GlobalStyle = createGlobalStyle`
 
     :focus {
         outline: 0;
-        box-shadow: 0 0 0 2px ${props => props.theme["green-500"]};
+        box-shadow: 0 0 0 2px ${(props) => props.theme["green-500"]};
     }
 
     body, input, text-area, button {
@@ -24,6 +24,6 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     
-  background-color: ${props => props.theme["gray-900"]};
-  color: ${props => props.theme["gray-300"]}
+  background-color: ${(props) => props.theme["gray-900"]};
+  color: ${(props) => props.theme["gray-300"]}
 `;
