8b4fc1814c3ccb764c21a20f7a8b04415d7d870e
TomDevBr <tomasjocalheiros@gmail.com>
2023-01-14 08:52:03 -0300

    Estrutura da aplicação - Tipagem de temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
+import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
-
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
-      <>
-      <Button variant={'primary'} />
-      <Button variant={'secondary'} />
-      <Button variant={'danger'} />
-      <Button variant={'success'} /></>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant={"primary"} />
+      <Button variant={"secondary"} />
+      <Button variant={"danger"} />
+      <Button variant={"success"} />
+    </ThemeProvider>
+  );
 }
-
-
