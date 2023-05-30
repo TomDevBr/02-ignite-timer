e816634326e5ac80fd758fc59b6d5cdb8c5973e3
TomDevBr <tomasjocalheiros@gmail.com>
2023-05-29 21:43:57 -0300

    Páginas e rotas - Componente: Status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,49 +1,48 @@
-import { HistoryContainer, HistoryList } from "./style";
+import { HistoryContainer, HistoryList, Status } from "./style";
 
 export function History() {
-    return (
-      <HistoryContainer>
-        <h1>Meu Hístorico</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu Hístorico</h1>
 
-        <HistoryList>
-          <table>
-            <thead>
+      <HistoryList>
+        <table>
+          <thead>
             <tr>
               <th>Tarefa</th>
               <th>Duração</th>
               <th>Início</th>
               <th>Status</th>
             </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Tarefa</td>
-                <td>20 minutos</td>
-                <td>Há 2 meses</td>
-                <td>Concluido</td>
-              </tr>
-              <tr>
-                <td>Tarefa</td>
-                <td>20 minutos</td>
-                <td>Há 2 meses</td>
-                <td>Concluido</td>
-              </tr>
-              <tr>
-                <td>Tarefa</td>
-                <td>20 minutos</td>
-                <td>Há 2 meses</td>
-                <td>Concluido</td>
-              </tr>
-              <tr>
-                <td>Tarefa</td>
-                <td>20 minutos</td>
-                <td>Há 2 meses</td>
-                <td>Concluido</td>
-              </tr>
-            </tbody>
-          </table>
-        </HistoryList>
-      </HistoryContainer>
-    )
-  }
-  
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td><Status statusColor="green">Concluído</Status></td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td><Status statusColor="yellow">Em andamento</Status></td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td><Status statusColor="red">interrompido</Status></td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td><Status statusColor="green">Concluído</Status></td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
