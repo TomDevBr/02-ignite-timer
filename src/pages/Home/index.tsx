85b776b06b3c924eef1c00e583983735757763d9
TomDevBr <tomasjocalheiros@gmail.com>
2023-06-13 22:03:23 -0300

    Funcionalidades da aplicação - Reduzindo countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { differenceInSeconds } from 'date-fns'
 import * as zod from 'zod'
 import {
   CountdownContainer,
@@ -11,7 +12,7 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './style'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -32,11 +33,12 @@ interface Cycle {
   id: string;
   task: string;
   minutesAmount: number;
+  startDate: Date;
 }
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([]);
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
-  const [amountSecondsPassed, setAmountSecondsPasses] = useState(0);
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
 
   const { register, handleSubmit, watch, reset} = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
@@ -55,6 +57,7 @@ export function Home() {
     id,
     task: data.task,
     minutesAmount: data.minutesAmount,
+    startDate: new Date(),
    };
 
    setCycles( state => ([...state, newCycle]));
@@ -62,13 +65,24 @@ export function Home() {
    reset();
   }
 
-
-
   const activeCycle = cycles.find(cycles => cycles.id === activeCycleId);
+    useEffect(()=>{
+      if(activeCycle){
+        setInterval(()=>{
+          setAmountSecondsPassed(
+            differenceInSeconds(new Date(), activeCycle.startDate)
+            )
+        }, 1000)
+      }
+    },[activeCycle])
+
+
+
+
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
   const minutesAmount = Math.floor(currentSeconds / 60);
-  const secondsAmount = minutesAmount % 60;
+  const secondsAmount = currentSeconds % 60;
 
   const minutes = String(minutesAmount).padStart(2, '0');
   const seconds = String(secondsAmount).padStart(2, '0');
